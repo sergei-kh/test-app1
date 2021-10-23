6f395528a8ade1eff6afee52c0dd67320381651c
SergeyKhlopov <sergey.program.com@yandex.ru>
2021-10-23 14:29:14 +0300

    feat(layout.report): Add layout page

--- resources/views/page/report.blade.php
+++ resources/views/page/report.blade.php
@@ -2,7 +2,21 @@
 @section('title', 'Отчёты')
 @section('caption', 'Отчёты')
 @section('content')
-    <form method="post" action="" class="mt-3" id="order_form">
-        <button type="submit" class="btn btn-primary">Сформировать отчёт</button>
+    <form method="get" action="{{route('report.index')}}" class="mt-3 row align-items-end">
+        <div class="col-md-4">
+            <label for="date_completed" class="form-label">Выберите дату</label>
+            <input type="date" name="date_completed" class="form-control" id="date_completed">
+        </div>
+        <div class="col-md-4">
+            <button type="submit" class="btn btn-primary">Сформировать отчёт</button>
+        </div>
     </form>
+    <div class="mt-4 row">
+        <div class="mb-3">
+            <h3>Количество выполненных заказов: 0 шт.</h3>
+        </div>
+        <div class="mb-3">
+            <h3>Общая сумма: 0 руб.</h3>
+        </div>
+    </div>
 @endsection
